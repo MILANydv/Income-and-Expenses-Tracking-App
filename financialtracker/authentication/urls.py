5546bc7a447f4bcd5bb4788a066bee73974c8253
Milan <ymilan361@gmail.com>
2021-08-11 10:08:21 +0545

    Authentication, Expenses app created

---
+++ financialtracker/authentication/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import *
+from django.views.decorators.csrf import csrf_exempt
+
+urlpatterns = [
+    path('register/', RegistrationView.as_view(), name = "register"),
+
+    path('validate-username',csrf_exempt( UsernameValidation.as_view()), name = "validate-username"),
+
+    path('login', LoginView.as_view(), name="login"),
+
+    path('logout', LogoutView.as_view(), name="logout"),
+   
+    path('validate-email', csrf_exempt(EmailValidationView.as_view()),
+         name='validate_email'),
+         
+    path('activate/<uidb64>/<token>',
+         VerificationView.as_view(), name='activate'),
+
+]
